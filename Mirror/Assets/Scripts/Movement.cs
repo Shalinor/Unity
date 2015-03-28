170a51d9a771a1c1429fbcf6923160dfc2fd1907
Shalinor <shalinor@internode.on.net>
2015-03-29 09:38:27 +1100

    Added movement and rotation to bolt.
    Needs spawning - random pos atm, aimed at center of course...
    
    Also need to test interaction with mirror...

---
+++ Mirror/Assets/Scripts/Movement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float thrust;
+//	private Rigidbody rigidbody;
+
+	void Start() {
+//		rigidbody = GetComponent<Rigidbody>();
+
+		GetComponent<Rigidbody>().AddForce(transform.forward * thrust);
+	}
+
+	void FixedUpdate() {
+	}
+}
---
+++ Mirror/Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+	public float rotationSpeed;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3(15, 30, 45) * Time.deltaTime * rotationSpeed);
+	}
+}
