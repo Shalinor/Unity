fa86600cc503b9b065ac972447ff7c6a1e99d7d8
Shalinor <shalinor@internode.on.net>
2016-03-17 06:02:00 +1100

    Adding Mighty Empires - at this point, just an implementation of Quill18's Simple Hex Based Game Design Tutorial...

---
+++ Mighty Empires/Assets/Scripts/Hex.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hex : MonoBehaviour {
+
+	// Our coordinates in the map array.
+	public int x;
+	public int z;
+
+	public Hex[] GetNeighbours()
+	{
+		// So if we are at x, z -- the neighbour to our left is at x-1, z
+
+		GameObject leftNeighbour = GameObject.Find("Hex_" + (x-1) + "_" + z);
+
+		// Right neighbour is also easy to find.
+		GameObject rightNeighbour = GameObject.Find("Hex_" + (x+1) + "_" + z);
+
+		// The problem is that our neighbours to our upper-left and upper-right
+		// might be at x-1 and x, OR they might be at x and x+1, depending
+		// on whether we ar on an even or odd row (i.e. if z % 2 is 0 or 1)
+
+		return null;
+
+	}
+
+}
