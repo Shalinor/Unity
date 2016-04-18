2da136818719d338226f29ce995a6eaf9117045a
Shalinor <shalinor@internode.on.net>
2016-04-18 12:58:20 +1000

    Almost finished 7 :)

---
+++ Project Porcupine/Assets/UI/AutomaticVerticalSize.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[ExecuteInEditMode]	//Naive way to run in editor... doesn't update (unless call is in void Update(){}), but is constantly running while editing. Button is a better method.
+public class AutomaticVerticalSize : MonoBehaviour {
+
+	public float childHeight = 35f;
+	public float childWidth = 150f;
+
+	// Use this for initialization
+	void Start () {
+		AdjustSize();
+	}
+	
+	public void AdjustSize()
+	{
+		Vector2 size = this.GetComponent<RectTransform>().sizeDelta;
+		size.x = childWidth;
+		size.y = this.transform.childCount * childHeight;
+		this.GetComponent<RectTransform>().sizeDelta = size;
+	}
+}
---
+++ Project Porcupine/Assets/UI/Editor/AutomaticVerticalSizeEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(AutomaticVerticalSize))]
+public class AutomaticVerticalSizeEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		if( GUILayout.Button("Recalc Size") )
+		{
+			//Equivalent - we are casting target to the script, which we can then call public functions, etc
+/*			AutomaticVerticalSize myScript = ((AutomaticVerticalSize)target);
+			myScript.AdjustSize();*/
+
+			((AutomaticVerticalSize)target).AdjustSize();
+		}
+	}
+}
